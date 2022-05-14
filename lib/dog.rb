186ce646d49d62c9ba0a008c5d6abb78a1631ef7
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-05-14 19:37:06 -0400

    commit tree cfeb62093a0c563dd4c3b2ed666a8b172c430ad1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    describe ".create_table" do
+        it 'creates the dogs table in the database' do
+          DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+          Dog.create_table
+          table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
+          expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
+        end
+      end
 
 end
